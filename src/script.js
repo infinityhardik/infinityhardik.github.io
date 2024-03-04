492c8250714fb7a6882eac9d77543524a7553662
Hardik Bhaavani <infinityhardik@users.noreply.github.com>
2024-03-04 11:02:45 +0530

    Updated sendOrder function

--- src/script.js
+++ src/script.js
@@ -1,18 +1,22 @@
-// Function to send order (replace with your implementation)
 function sendOrder() {
-
     // Check if the quantity is greater than zero
     if (parseInt(currentQuantity, 10) === 0 || selectedProducts.length === 0) {
         console.warn('Quantity is zero or no products in the order. Please add products to the order.');
         return;
     }
 
-    // Sort the selected products by Group Alias name first, then by Product Name
+    // Sort the selected products by Group Alias name first, then by Prod. Category, and finally by Product Name
     selectedProducts.sort((a, b) => {
         const groupComparison = a.product['Group Alias'].localeCompare(b.product['Group Alias']);
 
         if (groupComparison === 0) {
-            return a.product.Product.localeCompare(b.product.Product);
+            const categoryComparison = a.product['Prod. Category'].localeCompare(b.product['Prod. Category']);
+
+            if (categoryComparison === 0) {
+                return a.product.Product.localeCompare(b.product.Product);
+            }
+
+            return categoryComparison;
         }
 
         return groupComparison;
@@ -50,6 +54,7 @@ function sendOrder() {
     // Open the link in a new tab/window
     window.open(waMeLink, '_blank');
 }
+
 
 // Function to calculate the total quantity
 function calculateTotalQuantity() {
